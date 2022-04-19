8cd79d0bb4cdfc1acfa88e8a2aaf872698be4bf1
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-19 09:51:18 -0500

    added notes

--- Async/Program.cs
+++ Async/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+/*
+ In a synchronous program execution model,
+
+program is executed line by line, one at a time.
+
+When a function is called,
+
+programming execution has to wait until
+
+the function returns before it continues execution to the next line.
+ 
+ */
