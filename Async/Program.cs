4ec93248a615d1eab41d86346a707a02042abcca
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-19 10:00:53 -0500

    updates notes

--- Async/Program.cs
+++ Async/Program.cs
@@ -1,12 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using System;
+
+namespace Async // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
 
 
 
 
 
+/* 
+ *
+ * Synchroonous Program Execution
 
-/*
  In a synchronous program execution model,
 
 program is executed line by line, one at a time.
@@ -16,5 +31,11 @@ When a function is called,
 programming execution has to wait until
 
 the function returns before it continues execution to the next line.
+
+
+* When a function is called, program execution continues to th enext line, without waiting for the function to complete.
+* Asynchronous programming model improves responsiveness of your application.
+*
+*
  
  */
